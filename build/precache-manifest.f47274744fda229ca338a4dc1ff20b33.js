a2784b22df43e3e4989fb7dd44d185e66cfe66e1
quốc cường hồ <cuongseven789@gmail.com>
2020-06-08 21:23:25 +0700

    [U] git push

---
+++ build/precache-manifest.f47274744fda229ca338a4dc1ff20b33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfae942dd06f83baf76d5c8c06eb2ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6856e4254e7277d61956",
+    "url": "/static/css/2.b29cfbc5.chunk.css"
+  },
+  {
+    "revision": "f91982c82029365d1812",
+    "url": "/static/css/main.4d2ba9f0.chunk.css"
+  },
+  {
+    "revision": "6856e4254e7277d61956",
+    "url": "/static/js/2.a0beca26.chunk.js"
+  },
+  {
+    "revision": "e3fb4930329a33a6bafdf0d9f304241e",
+    "url": "/static/js/2.a0beca26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91982c82029365d1812",
+    "url": "/static/js/main.274165b3.chunk.js"
+  },
+  {
+    "revision": "96c6a1c301bd39402041",
+    "url": "/static/js/runtime-main.3f951493.js"
+  }
+]);
