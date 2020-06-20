a85cbc0bfcdb6419277c23a10cece5cef561a1d8
quốc cường hồ <cuongseven789@gmail.com>
2020-06-20 18:20:17 +0700

    [U] code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -579,6 +579,21 @@ class Home extends Component {
     })
   }
 
+  randDarkColor() {
+    var lum = -0.25;
+    var hex = String('#' + Math.random().toString(16).slice(2, 8).toUpperCase()).replace(/[^0-9a-f]/gi, '');
+    if (hex.length < 6) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    var rgb = "#",
+      c, i;
+    for (i = 0; i < 3; i++) {
+      c = parseInt(hex.substr(i * 2, 2), 16);
+      c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+      rgb += ("00" + c).substr(c.length);
+    }
+    return rgb;
+  }
 
   render() {
     const { titleFolder, data, isPublic, privacyShow, photoIndexPublic, photoIndexPrivate, isOpenPrivate, isOpenPublic, toggle, showEdit, itemEdit, userShare, userList, emailLink, showAdd, description } = this.state
@@ -1101,7 +1116,10 @@ class Home extends Component {
                   patientName: arrayHost[0],
                   file: itemEdit,
                   email: window.localStorage.getItem('email'),
-                  userId: window.localStorage.getItem('id')
+                  userId: window.localStorage.getItem('id'),
+                  color: this.randDarkColor(),
+                  nickname: arrayHost[0],
+                  aboutMe: '',
                 })
 
                 userService.connectionsUsers({ connection: { to: userShare } }).then(result => {
